c386ae970abc458d0195a090cef29ebb910ab1ec
Me Panda <me@ipanda.ru>
2019-05-26 10:32:46 +0300

    push.js socketio client configs

--- node/push.js
+++ node/push.js
@@ -1,34 +1,76 @@
-// Dependencies
-const fs = require('fs');
+
+
 const http = require('http');
-const https = require('https');
+const path=require("path");
 const express = require('express');
-
+const socketio = require("socket.io");
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
-// Certificate
-const privateKey = fs.readFileSync('/etc/letsencrypt/live/hg.hatarisu.ru/privkey.pem', 'utf8');
-const certificate = fs.readFileSync('/etc/letsencrypt/live/hg.hatarisu.ru/cert.pem', 'utf8');
-const ca = fs.readFileSync('/etc/letsencrypt/live/hg.hatarisu.ru/chain.pem', 'utf8');
+const port = 8080;
+const publicFolder=path.join(__dirname,"/publicFolder");
+app.use(express.static(publicFolder));
 
-const credentials = {
-    key: privateKey,
-    cert: certificate,
-    ca: ca
-};
-
-app.use((req, res) => {
-    res.send('Hello there !');
+io.on("connection",()=>{
+    console.log("client is connected");
+    setInterval(()=>{
+        console.log(Math.round((new Date())/1000)+" Event is emitted");
+        
+        io.emit("push", { "TS":"1559907920","text":"7 июня отключение горячей воды"});
+    },30000);
 });
 
-// Starting both http & https servers
-const httpServer = http.createServer(app);
-const httpsServer = https.createServer(credentials, app);
+server.listen(port,()=>{
+    console.log("Server is up on 8080 port");
+    
+});
 
-// httpServer.listen(8080, () => {
-//     console.log('HTTP Server running on port 80');
+// // Dependencies
+// const fs = require('fs');
+// const http = require('http');
+// const https = require('https');
+// const express = require('express');
+// const socketio = require("socket.io");
+// const path=require("path");
+
+// const publicFolder=path.join(__dirname,"/publicFolder");
+
+// // Certificate
+// const privateKey = fs.readFileSync('/etc/letsencrypt/live/hg.hatarisu.ru/privkey.pem', 'utf8');
+// const certificate = fs.readFileSync('/etc/letsencrypt/live/hg.hatarisu.ru/cert.pem', 'utf8');
+// const ca = fs.readFileSync('/etc/letsencrypt/live/hg.hatarisu.ru/chain.pem', 'utf8');
+
+// const credentials = {
+//     key: privateKey,
+//     cert: certificate,
+//     ca: ca
+// };
+// const app = express();
+
+
+
+// // Starting both http & https servers
+// const httpServer = http.createServer(app);
+// const httpsServer = https.createServer(credentials, app);
+// const io = socketio(httpServer);
+// // httpServer.listen(8080, () => {
+// //     console.log('HTTP Server running on port 80');
+// // });
+
+// app.use(express.static(publicFolder));
+// app.use((req, res) => {
+//     res.send('Hello there !');
 // });
 
-httpsServer.listen(8043, () => {
-    console.log('HTTPS Server running on port 8043');
-});
+// io.on("connection",()=>{
+//     console.log("Socket io client is connected");
+    
+// });
+
+
+// httpServer.listen(8080, () => {
+//     console.log('HTTPS Server running on port 8080');
+//     console.log(`Serving static from ${publicFolder}`);
+// });
+
